ebcca419a9b8fd3d452ac3373c45c916a22a6f32
JP <jpniznhy@hotmail.com>
2023-11-06 18:24:35 +0100

    Add SparkConf and SparkSession to process function

--- Documentation/Manuals/TrainingCode/Exercise1.py
+++ Documentation/Manuals/TrainingCode/Exercise1.py
@@ -1,3 +1,23 @@
-import pyspark
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+
+sc = SparkConf()\
+    .set("spark.master", "local")\
+        .set("spark.app.name", "DBALab")
+
 
 def process(sc):
+    spark = SparkSession.builder.config(conf=sc).getOrCreate()
+
+    df = spark.read.csv('resources/bank.csv', header=True)
+    # df.show()
+
+    df.createOrReplaceTempView("bank")
+    spark.sql('SELECT job, loan, AVG(balance) FROM bank GROUP BY job, loan ORDER BY job, loan').show()
+
+    
+
+    spark.stop()
+
+process(sc)
