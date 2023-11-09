c8bd9fad6f683ce29cffa14aa8168e7950147519
JP <jpniznhy@hotmail.com>
2023-11-09 10:59:30 +0100

    Order matrix by aircraft id and day in management pipeline

--- CodeSkeleton/management_pipeline.py
+++ CodeSkeleton/management_pipeline.py
@@ -112,6 +112,7 @@ def managment_pipe(filepath: str, spark: SparkSession, dbw_properties: dict, dam
 
     matrix = matrix.join(labels, (matrix['aircraft id'] == labels['aircraftregistration']) & (matrix['day'] == labels['date']), 'inner').drop('aircraftregistration', 'date')
 
+    matrix = matrix.orderBy('aircraft id', 'day')
 
     print(f'{Fore.GREEN}End of the Managment Pipeline{Fore.RESET}' + '\n' + '-'*50)
 
